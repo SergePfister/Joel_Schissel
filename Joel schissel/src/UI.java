7ec3b7c7fee47d5398a4a2094a183ab32b42e3ba
Serge Pfister <serge.pfister@outlook.de>
2021-08-16 20:23:04 +0200

    updated ui

--- Joel schissel/src/UI.java
+++ Joel schissel/src/UI.java
@@ -1,6 +1,9 @@
 package src;
+
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -10,12 +13,14 @@ import javafx.scene.layout.VBox;
 import javafx.util.converter.NumberStringConverter;
 
 public class UI extends VBox {
+    static boolean staticBol;
+    StringProperty statusProp;
+    Label status;
     ImageFinder imagefinder;
     boolean runBol;
     Slider inaccuracy;
     HBox hBox1;
     HBox hBox2;
-    HBox hBox3;
     Button run;
     Button stop;
     Button restart;
@@ -37,11 +42,11 @@ public class UI extends VBox {
 
         run.setMinSize(width / 3, height / 6);
         stop.setMinSize(width / 3, height / 6);
-        restart.setMinSize(width / 3, height / 6);
+        status.setMinSize(width / 3, height / 6);
 
         run.setPadding(new Insets(insets));
         stop.setPadding(new Insets(insets));
-        restart.setPadding(new Insets(insets));
+        status.setPadding(new Insets(insets));
 
         inaccuracy.setShowTickMarks(true);
         inaccuracy.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
@@ -55,31 +60,37 @@ public class UI extends VBox {
         labeltxt.setPadding(new Insets(insets));
         labeltxt.setMinSize(width / 2, height / 6);
 
-        hBox1.getChildren().addAll(run, stop, restart);
+        hBox1.getChildren().addAll(run, stop, status);
         hBox2.getChildren().addAll(labeltxt, label);
         this.getChildren().addAll(hBox1, hBox2, inaccuracy);
 
     }
-    public void foo()  
-    {
+
+    public void foo() {
         Thread t = new Thread(new Runnable() {
             public void run() {
+                boolean found = false;
                 try {
-                    while (!imagefinder.runner()&&runBol) {}
+                    while (!found && runBol) {
+                        found = imagefinder.runner();
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-           }
-       });
-       t.start();
+            }
+        });
+        t.start();
     }
 
     private void initializeControlls() {
+        staticBol = false;
+        statusProp = new SimpleStringProperty("Stoped");
+        status = new Label();
+        status.textProperty().bindBidirectional(statusProp);
         labeltxt = new Label("Inaccuracy");
         label = new Label(0.0 + "");
         hBox1 = new HBox();
         hBox2 = new HBox();
-        hBox3 = new HBox();
         inaccuracy = new Slider(0, 101, 0);
         run = new Button("Run");
         stop = new Button("Stop");
@@ -92,12 +103,18 @@ public class UI extends VBox {
             if (runBol) {
                 System.out.println("im still running");
             } else {
+                staticBol = false;
+                statusProp.set("Running");
                 runBol = true;
                 foo();
+                if(staticBol){
+                    statusProp.set("Found");
+                }
             }
         });
         stop.setOnAction(e -> {
             if (runBol) {
+                statusProp.set("Stoped");
                 runBol = false;
             }
         });
